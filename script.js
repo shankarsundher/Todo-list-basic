7e6f006df117b1086191b1f95b9784ea5358d903
Shankar <shankarsundher21@gmail.com>
2024-02-09 23:41:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert('Write something in the text box!');
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function (i) {
+    if (i.target.tagName === "LI") {
+        i.target.classList.toggle("checked")
+        saveData();
+    }
+    else if (i.target.tagName === "SPAN") {
+        i.target.parentElement.remove();
+        saveData();
+    }
+}, false)
+
+function saveData() {
+    localStorage.setItem("tasks", listContainer.innerHTML);
+}
+function showTasks() {
+    listContainer.innerHTML = localStorage.getItem("tasks");
+}
+showTasks();
